0575b551817814c7a39da0f472c7b5faaaf8f1fe
Bhargawanan Bhuyan <bhargawanan@outlook.com>
2022-05-17 04:03:06 +0530

    added login logic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,5 @@
+import axios from 'axios'
+
 export const url = 'https://creatilat.azurewebsites.net'
+
+export const apiService = axios.create({ baseURL: 'https://creatilat.azurewebsites.net/api' })
